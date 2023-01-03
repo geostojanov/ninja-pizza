65057d19d31a9f5a433be24bc70d3f88f0232b8a
geostojanov <gestojanov@gmail.com>
2023-01-03 08:54:05 +0100

    Basic Form Validation

--- add.php
+++ add.php
@@ -7,10 +7,29 @@
 //  }
 
   if(isset($_POST['submit'])) {
-    echo $_POST['email'] . '<br />';
-    echo $_POST['title'] . '<br />';
-    echo $_POST['ingredients'] . '<br />';
-  }
+
+    // check email
+    if (empty($_POST['email'])) {
+      echo 'An email is required <br />';
+    } else {
+      echo htmlspecialchars($_POST['email']);
+    }
+
+    // check title
+    if (empty($_POST['title'])) {
+      echo 'A title is required <br />';
+    } else {
+      echo htmlspecialchars($_POST['title']);
+    }
+
+    // check ingredients
+    if (empty($_POST['ingredients'])) {
+      echo 'At least one ingredient is required <br />';
+    } else {
+      echo htmlspecialchars($_POST['ingredients']);
+    }
+
+  } // end of POST check
 
 
 ?>
