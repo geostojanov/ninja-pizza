adeb3dce36e4fc11bcdbe646e876457eda2e5e8d
geostojanov <gestojanov@gmail.com>
2023-01-03 15:50:35 +0100

    connecting to database

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+  // Connect to database
+  $conn = mysqli_connect('localhost', 'george', 'rootPassword!!',
+    'ninja_pizza');
+
+  if (!$conn) {
+    echo 'Connection error: ' . mysqli_connect_error();
+  }
+
+
+
 ?>
 
 <!doctype html>
