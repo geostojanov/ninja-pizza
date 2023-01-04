1b2e4eb891bd6961618db07fd4f010a74d5d5f15
geostojanov <gestojanov@gmail.com>
2023-01-04 14:33:36 +0100

    finishing touches

--- details.php
+++ details.php
@@ -2,6 +2,19 @@
 
   include('config/db_connect.php');
 
+  if (isset($_POST['delete'])) {
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+    $sql = "delete from pizzas where id = $id_to_delete";
+
+    if(mysqli_query($conn, $sql)) {
+      // success
+      header('Location: index.php');
+    } {
+      echo 'query error: ' . mysqli_error($conn);
+    }
+  }
+
   // check GET request
   if (isset($_GET['id'])) {
 
@@ -27,7 +40,7 @@
 <html lang="en">
   <?php include('templates/header.php'); ?>
 
-  <div class="container center">
+  <div class="container center grey-text">
     <?php if($pizza) : ?>
 
       <h4><?php echo htmlspecialchars($pizza['title']); ?></h4>
@@ -36,6 +49,11 @@
 
       <h5>Ingredients:</h5>
       <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+      <!--Delete form -->
+      <form action="details.php" method="POST">
+        <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id'] ?>">
+        <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+      </form>
 
     <?php else : ?>
 
