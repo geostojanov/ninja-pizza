74b23fb97cc08546142fdc7efc3dff6df28e2503
geostojanov <gestojanov@gmail.com>
2023-01-04 07:22:05 +0100

    fix

--- index.php
+++ index.php
@@ -41,8 +41,8 @@
         <div class="col s6 md3">
           <div class="card z-depth-0">
             <div class="card-content center">
-              <h6><?php echo $pizza['title']; ?></h6>
-              <div><?php echo $pizza['ingredients']; ?></div>
+              <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+              <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
             </div>
             <div class="card-action right-align">
               <a href="#" class="brand-text">more info</a>
